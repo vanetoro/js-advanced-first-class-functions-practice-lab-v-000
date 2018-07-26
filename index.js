4dfef32d21a7dd10ebaeaa13a76032dbda653eb7
Vanessa Toro <vanetoro84@gmail.com>
2018-07-26 19:58:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function logDriverNames(array){
+  array.forEach(function(driver_name){
+    console.log(driver_name.name);
+  })
+}
+
+function logDriversByHometown(array, location){
+  array.forEach(function(driver){
+    if (driver.hometown == location){
+      console.log(driver.name);
+    }
+  })
+}
+
+// function driversByRevenue(array){
+//   array.sort
+//   console.log(array)
+// }
